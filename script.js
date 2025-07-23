107bdad95160887e1cd8d0508380c2ddab5d2587
Lorenzo <95371347+Lorenzo2013460@users.noreply.github.com>
2025-07-23 20:41:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Benvenuto nel sito WIP di Lorenzo!");
+  const dropdowns = document.querySelectorAll(".dropdown");
+
+  dropdowns.forEach(dropdown => {
+    const btn = dropdown.querySelector(".dropbtn");
+
+    btn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      closeAllDropdowns();
+      dropdown.classList.toggle("active");
+    });
+  });
+
+  document.addEventListener("click", closeAllDropdowns);
+
+  function closeAllDropdowns() {
+    dropdowns.forEach(drop => drop.classList.remove("active"));
+  }
 });
